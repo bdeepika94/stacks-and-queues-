95128d80abc33ed2c19718bba26071e4805fee14
bdeepika94 <54839801+bdeepika94@users.noreply.github.com>
2019-10-11 23:14:46 +0530

    Update main_s.c

--- main_s.c
+++ main_s.c
@@ -118,7 +118,146 @@ assert(res.data=='6');
 //printf(stk->data[stk->top]);
 
 }
+void constant()
+{
+Queue k= queue_new(10);
+Queue *q=&k;
+Queue_Result Qres;
 
+q=queue_add(q,10,&Qres);
+q=queue_add(q,20,&Qres);
+q=queue_add(q,30,&Qres);
+q=queue_add(q,40,&Qres);
+
+assert(q->count==4);
+int d2count=1;
+int d3count=q->count;
+
+while(d2count<=d3count)
+{
+int dcount=1;
+while(dcount<q->count)
+{
+	q = queue_delete(q,&Qres);
+	q = queue_add(q,Qres.data,&Qres);
+	dcount++;
+}
+	q = queue_delete(q,&Qres);
+
+d2count++;
+}
+assert(Qres.data==10);
+}
+
+void stack_toqueue()
+{
+	Stack stk = stack_new(10);
+Stack *s=&stk;
+Stack_Result Sres;
+
+Queue k= queue_new(10);
+Queue *q=&k;
+Queue_Result Qres;
+
+s = stack_push(s,93,&Sres);
+s = stack_push(s,53,&Sres);
+s = stack_push(s,73,&Sres);
+s = stack_push(s,83,&Sres);
+
+int element = 53;
+int count = 0;
+int i=0;
+while(i<=s->top+1)
+{
+s = stack_peek(s,&Sres);
+if(Sres.data==element)
+{
+	break;
+}
+else
+{
+	s = stack_pop(s,&Sres);
+	q = queue_add(q,Sres.data,&Qres);
+	count++;
+}
+i++;
+}
+
+int dcount=count;
+int d1count=count;
+while(count!=0)
+{
+	q= queue_delete(q,&Qres);
+	s= stack_push(s,Qres.data,&Sres);
+	count--;
+}
+s = stack_peek(s,&Sres);
+assert(Sres.data==73);
+
+while(dcount!=0)
+{
+	s=stack_pop(s,&Sres);
+	q=queue_add(q,Sres.data,&Qres);
+	dcount--;
+}
+
+s = stack_peek(s,&Sres);
+assert(Sres.data==53);
+
+
+while(d1count!=0)
+{
+	q= queue_delete(q,&Qres);
+	s= stack_push(s,Qres.data,&Sres);
+	d1count--;
+}
+
+s = stack_peek(s,&Sres);
+assert(Sres.data==83);
+
+}
+
+/*Stack* addQueue(Stack *s1,int32_t element,Stack_Result Sres1)
+{
+
+	s1=stack_push(s1,element,&Sres1);
+	return s1;
+}
+*/
+Stack* deleteQueue(Stack *s1,Stack *s2,Stack_Result Sres1,Stack_Result Sres2)
+{
+	while(s1->top>-1)
+	{
+	s1 = stack_pop(s1,&Sres1);
+	int32_t popelement = Sres1.data;
+	s2 = stack_push(s2,popelement,&Sres2);
+	}
+	s2 = stack_pop(s2,&Sres2);
+	return s2;
+}
+
+
+void queueUsingStacks()
+{
+	Stack stk1 = stack_new(10);
+	Stack *s1=&stk1;
+	Stack_Result Sres1;
+
+	s1 = stack_push(s1,10,&Sres1); //it takes only one time to push the values
+	s1 = stack_push(s1,20,&Sres1);
+	s1 = stack_push(s1,50,&Sres1);
+
+	Stack stk2 = stack_new(10);
+	Stack *s2=&stk2;
+	Stack_Result Sres2;
+
+
+	s2 = deleteQueue(s1,s2,Sres1,Sres2);
+	s1 = stack_peek(s1,&Sres1);
+	assert(s1->top==-1);
+
+	s2 = stack_peek(s2,&Sres2);
+	assert(Sres2.data==20);
 int main()
 {
 	//1.Check balancing of symbols ( like ‘{‘, ‘(‘, ‘[‘ ) and their order using stack.
@@ -132,6 +271,9 @@ int main()
     	char E[20]="423+1*+3-";
 
 	postfix_stk(E);
+	constant();
+       stack_toqueue();
+	queueUsingStacks();
 	return 0;
 
 }
